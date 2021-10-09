95108ba1e41eb08cd2d63d50ea71ca0cd6bfb1bc
Vigor-Lundkvist <vigor.lundkvist@live.se>
2021-10-10 00:33:30 +0200

    Slight modifications and fixed undefined on image src.

--- public/js/app.js
+++ public/js/app.js
@@ -16,10 +16,14 @@ console.log('hey hey from client');
             const data = req.response;
             console.log(data);
 
-            //Data
-            const headline = data['headline']; //data from json
-            const intro = data['intro'];       //data from json
+            //DATA
             
+            //const headline = data['headline']; //data from json
+            //const intro = data['intro'];       //data from json
+            //can also call the attribute data directly by doing 
+            //p1.textContent = data.intro instead of setting a const up for data.
+
+
             //Elements
             const img = document.createElement('img');
             const p1 = document.createElement('p');
@@ -27,13 +31,18 @@ console.log('hey hey from client');
             const button = document.createElement('button');
 
             //Only works after creating the element
-            img.src = data['articleimage']; 
+            //img.src = data['articleimage']; didn't work and gave undefined
+            img.setAttribute('src', data.articleImage);
             
-            p1.textContent = intro;
-            h1.textContent = headline;
+            //can also call the attribute data directly by doing 
+            //p1.textContent = data.intro instead of seting a const up at data.
+            p1.textContent = data.intro; 
+            h1.textContent = data.headline;
             button.textContent = "hide";
 
-            function myFunction() {
+            //can also use: button.addEventListener('click', Button1Handler);
+            button.onclick = Button1Handler; //Button for hiding image.
+            function Button1Handler() {
                 var x = document.getElementById("Exercise2");
                 if (x.style.display === "none") {
                   x.style.display = "block";
@@ -43,7 +52,7 @@ console.log('hey hey from client');
                   button.textContent = "unhide";
                 }
               }
-              button.onclick = myFunction;
+              
 
             document.getElementById('Exercise3').appendChild(button);      
             document.getElementById('Exercise1').appendChild(h1);
--- public/js/edit.js
+++ public/js/edit.js
@@ -16,15 +16,15 @@ req.onload = () => {
     const form = document.createElement('form')
     const tBox1 = document.createElement('input');
     const tBox2 = document.createElement('input');
-    //const tBox3 = document.createElement('input');
+    const tBox3 = document.createElement('input');
     const btnUpdate = document.createElement('button');
 
     tBox1.id = 'tBoxId1';
     tBox2.id = 'tBoxId2';
-    //tBox3.id = 'tBoxId3';
+    tBox3.id = 'tBoxId3';
     tBox1.name = 'tBoxName1';
     tBox2.name = 'tBoxName2';
-    //tBox3.name = 'tBoxName3';
+    tBox3.name = 'tBoxName3';
     btnUpdate.id = 'btnUpdateId';
     form.id = 'formId';
 
@@ -35,7 +35,7 @@ req.onload = () => {
 
     tBox1.value = data.headline;
     tBox2.value = data.intro;
-    //tBox3.value = data.articleImage;
+    tBox3.value = data.articleImage;
     btnUpdate.textContent = 'Update';
 
     btnUpdate.addEventListener('click', ButtonEventhandler);
@@ -44,14 +44,14 @@ req.onload = () => {
     document.getElementById('Exercise2').appendChild(form);
     document.getElementById('formId').appendChild(tBox1);
     document.getElementById('formId').appendChild(tBox2);
-    //document.getElementById('formId').appendChild(tBox3);
+    document.getElementById('formId').appendChild(tBox3);
     document.getElementById('formId').appendChild(btnUpdate);
 };
 
 const ButtonEventhandler = () => {
     const tBox1 = document.getElementById('tBoxId1');
     const tBox2 = document.getElementById('tBoxId2');
-    //const tBox3 = document.getElementById('tBoxId3');
+    const tBox3 = document.getElementById('tBoxId3');
     const btnUpdate = document.getElementById('btnUpdateId');
 
     console.log('Button Update pushed');
